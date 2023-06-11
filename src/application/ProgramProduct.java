f3cc4276fa28cdb6b2ee46552c301a8b46f7678f
Gabriel Machado <bieloliveiramachado@gmail.com>
2023-06-11 15:09:41 -0300

    With anonymous class

--- src/application/ProgramProduct.java
+++ src/application/ProgramProduct.java
@@ -1,9 +1,10 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
-import entities.MyComparator;
+//import entities.MyComparator;
 import entities.Products;
 
 public class ProgramProduct {
@@ -16,7 +17,15 @@ public class ProgramProduct {
 		list.add(new Products("Notebook", 1200.0));
 		list.add(new Products("Tablet", 450.0));
 
-		list.sort(new MyComparator());
+		Comparator<Products> comp = new Comparator<Products>() {
+			@Override
+			public int compare(Products o1, Products o2) {
+				return o1.getName().toUpperCase().compareTo(o2.getName().toUpperCase());
+			}
+		};
+		
+		
+		list.sort(comp);
 		
 		for(Products p : list) {
 			System.out.println(p);
