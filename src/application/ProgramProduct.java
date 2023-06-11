a13a25273012ed56c5bfd4e2f6c26b113968a0f5
Gabriel Machado <bieloliveiramachado@gmail.com>
2023-06-11 15:23:17 -0300

    Final lambda expression

--- src/application/ProgramProduct.java
+++ src/application/ProgramProduct.java
@@ -1,7 +1,7 @@
 package application;
 
 import java.util.ArrayList;
-import java.util.Comparator;
+//import java.util.Comparator;
 import java.util.List;
 
 //import entities.MyComparator;
@@ -16,10 +16,8 @@ public class ProgramProduct {
 		list.add(new Products("TV", 900.0));
 		list.add(new Products("Notebook", 1200.0));
 		list.add(new Products("Tablet", 450.0));
-
-		Comparator<Products> comp = (o1, o2) -> o1.getName().toUpperCase().compareTo(o2.getName().toUpperCase());
 		
-		list.sort(comp);
+		list.sort((o1, o2) -> o1.getName().toUpperCase().compareTo(o2.getName().toUpperCase()));
 		
 		for(Products p : list) {
 			System.out.println(p);
